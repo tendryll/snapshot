182818eec3fcb2d3b6a8310ccb611a34657f8143
Sean Alphonse <sean@aleksandr.cc>
2023-05-22 21:39:20 -0500

    fixed testing, set weather.service.url property

--- src/main/java/io/aleksandr/labs/snapshot/config/AppConfig.java
+++ src/main/java/io/aleksandr/labs/snapshot/config/AppConfig.java
@@ -1,5 +1,6 @@
 package io.aleksandr.labs.snapshot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +9,11 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+  @Value("${weather.service.url}")
+  private String weatherServiceUrl;
+
   @Bean
   public RestTemplate restTemplate() {
-    return new RestTemplateBuilder().rootUri("https://api.weather.gov").build();
+    return new RestTemplateBuilder().rootUri(weatherServiceUrl).build();
   }
 }
--- src/test/java/io/aleksandr/labs/snapshot/SpringIntegrationTest.java
+++ src/test/java/io/aleksandr/labs/snapshot/SpringIntegrationTest.java
@@ -2,8 +2,10 @@ package io.aleksandr.labs.snapshot;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = SnapshotApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("qa")
 public class SpringIntegrationTest {
 }
