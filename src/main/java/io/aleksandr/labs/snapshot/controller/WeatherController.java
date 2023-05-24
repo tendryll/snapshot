27dfa3013fd6745aa9ae6768222b1d59f5f4eb36
Sean Alphonse <sean@aleksandr.cc>
2023-05-24 07:46:09 -0500

    removed header

--- src/main/java/io/aleksandr/labs/snapshot/controller/WeatherController.java
+++ src/main/java/io/aleksandr/labs/snapshot/controller/WeatherController.java
@@ -3,7 +3,6 @@ package io.aleksandr.labs.snapshot.controller;
 import io.aleksandr.labs.snapshot.model.stations.observations.response.WeatherInfo;
 import io.aleksandr.labs.snapshot.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,10 +22,6 @@ class WeatherController {
 
   @GetMapping(value = "/{stationId}", produces = {"application/json"})
   ResponseEntity<WeatherInfo> get(@PathVariable String stationId) {
-//    final HttpHeaders responseHeaders = new HttpHeaders();
-//    responseHeaders.set("aleksandr.io", "sean@aleksandr.cc");
-
-    // return ResponseEntity.ok().headers(responseHeaders).body(weatherService.get(stationId));
     return ResponseEntity.ok().body(weatherService.get(stationId));
   }
 }
